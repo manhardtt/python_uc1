786bc8ba473b781999817893a1c7939312efef1d
manhardtt <joaopedrorochaschuenquer800@gmail.com>
2025-08-12 20:34:18 -0300

    Aula 08 - Desafio 08 - 12/08/2025

---
+++ aula08-atividade01-01.py
@@ -0,0 +1,27 @@
+##
+# Aula 08 - Atividade 01 - Exercicio 01
+# Nome: [João Pedro]
+#Data: 12/08/2025
+##
+
+"""
+Função Saudação
+     - Imprime uma mensagem de boas vindas
+"""
+def saudacao() :
+    print(f"\n\n\tOlá usuário, seja bem vindo !!\n\n")
+
+
+##
+# Corpo do Programa
+##
+saudacao()
+
+
+
+
+def soma_numeros (a, b):
+    return a + b
+# Exemplo de chamada
+resultado = soma_numeros(5, 7)
+print("A soma é:", resultado)

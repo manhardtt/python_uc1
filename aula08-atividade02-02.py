45aa8fea2800f1c112302a2af9f8739a7e93fba6
manhardtt <joaopedrorochaschuenquer800@gmail.com>
2025-08-12 21:49:19 -0300

    aula08-atividade02-02.py

---
+++ aula08-atividade02-02.py
@@ -0,0 +1,45 @@
+##
+# Aula 08 - Atividade 02 - Exercicio 02
+# Nome: [João Pedro]
+#Data: 12/08/2025
+##
+
+def saudacao() :
+    print(f"\n\n\tOlá usuário, seja bem vindo !!\n\n")
+def main():
+
+
+    print("Olá mundo!")
+    if __name__ == "__main__":
+        main()
+ 
+
+
+
+def main():
+   n1 = float(input("Digite o primeiro número: "))
+   n2 = float(input("Digite o segundo número: "))
+   soma = n1 + n2
+   print(f"A soma é: {soma}")
+
+def soma (a,b):
+    return a + b 
+
+def subtracao(a,b):
+    return a - b
+
+def calcula():
+    resultado_soma = soma(10,3)
+    resultado_subtracao = subtracao(10,3)
+    print((f"a soma dos dois numeros é {resultado_soma}"))
+    print((f"a subtração dos dois números é {resultado_subtracao}"))
+
+
+if __name__ == "__main__":
+
+    #saudacao()
+
+    #print(f"{somar_numeros(15,16)}")
+
+    #somar_numeros_v2 (15,16)
+    calcula ()
